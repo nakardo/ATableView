f3fdfcf8f84ce2256dd9dd6488d6bdb6a28031a4
Diego Acosta <diegonake@gmail.com>
2013-04-18 22:53:45 -0300

    closes #14

--- library/src/com/nakardo/atableview/internal/ATableViewCellClickListener.java
+++ library/src/com/nakardo/atableview/internal/ATableViewCellClickListener.java
@@ -3,6 +3,8 @@ package com.nakardo.atableview.internal;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.HeaderViewListAdapter;
+import android.widget.ListAdapter;
 
 import com.nakardo.atableview.protocol.ATableViewDelegate;
 import com.nakardo.atableview.view.ATableView;
@@ -17,6 +19,13 @@ public class ATableViewCellClickListener implements OnItemClickListener {
 	@Override
 	public void onItemClick(AdapterView<?> adapter, View view, int pos, long id) {
 		ATableViewAdapter a = mTableView.getInternalAdapter();
+		
+		// closes #14, adding a header to the table causes an offset on rows position.
+		ListAdapter rawAdapter = mTableView.getAdapter();
+		if (rawAdapter instanceof HeaderViewListAdapter &&
+			((HeaderViewListAdapter) rawAdapter).getHeadersCount() > 0) {
+			pos--;
+		}
 		
 		// do not send callbacks for header rows.
 		if (!a.isHeaderRow(pos) && !a.isFooterRow(pos)) {
